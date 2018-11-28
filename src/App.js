9baa13c70df3fb7adf8e77c0c3f0fc1fd65406a7
muraleo <muraleo80923@gmail.com>
2018-11-28 14:07:03 -0500

    finish navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<nav>
+						<ul>
+							<li>
+								<a href="/home">Home</a>
+							</li>
+							<li>
+								<a href="/">Login</a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
